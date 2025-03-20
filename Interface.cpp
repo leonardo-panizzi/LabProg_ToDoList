7fa0617577bd996d2916b3c494524186d089585a
Leonardo Panizzi <leonardo.panizziedu.unifi.it>
2025-03-20 16:42:04 +0100

    Added an "Interface" class with menu display functionality This class allows the user to press a specific key to perform an action, il will be visible once the program is launched allowing the user to choose. The user's choice will then be interpreted by a specific method in this same class.

---
+++ Interface.cpp
@@ -0,0 +1,13 @@
+//
+// Created by leona on 20/03/2025.
+//
+
+#include "Interface.h"
+
+void Interface::showMenu() {
+    cout << "\n[HOME]" << endl;
+    cout << "1. Add a task" << endl;
+    cout << "2. Remove a task" << endl;
+    cout << "3. Show all tasks" << endl;
+    cout << "4. Save and quit" << endl;
+}
---
+++ Interface.h
@@ -0,0 +1,16 @@
+//
+// Created by leona on 20/03/2025.
+//
+
+#ifndef PROGTODO_INTERFACE_H
+#define PROGTODO_INTERFACE_H
+#include <list>
+#include "Task.h"
+
+class Interface {
+public:
+    static void showMenu(); //method that displays a menu from which the user can perform an action
+};
+
+
+#endif //PROGTODO_INTERFACE_H
